3258c526ff1516379dc292c97dc766731e8338c8
vandiar <al.vandiar@gmail.com>
2018-02-04 07:29:02 +0700

    move directories

--- src/com/pagerank/Main.java
+++ src/com/pagerank/Main.java
@@ -1,10 +1,19 @@
-package com.PageRank;
+/*
+ * Copyright ${YEAR}.${NAME}
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
-import com.CalculationRank.SecondJobMapper;
-import com.CalculationRank.SecondJobReducer;
-import com.GraphParser.FirstJobMapper;
-import com.GraphParser.FirstJobReducer;
-import com.RankOrder.ThirdJobMapper;
+package com.pagerank;
+
+import com.pagerank.calculation.SecondJobMapper;
+import com.pagerank.calculation.SecondJobReducer;
+import com.pagerank.parse.FirstJobMapper;
+import com.pagerank.parse.FirstJobReducer;
+import com.pagerank.ranker.ThirdJobMapper;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
